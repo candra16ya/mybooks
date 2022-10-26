fc23feebaeb42a7391f8322b1bbd46dcd1936488
candzon <mf123cw@gmail.com>
2022-10-26 08:38:32 +0700

    🫶 update scaffold and .wrapContentWidth image

--- app/src/main/java/com/bacain/buku/screens/DetailsBook.kt
+++ app/src/main/java/com/bacain/buku/screens/DetailsBook.kt
@@ -1,10 +1,13 @@
 package com.bacain.buku.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,63 +28,80 @@ import com.bacain.buku.data.BookRepository
 import com.bacain.buku.ui.theme.Shapes
 import com.bacain.buku.ui.theme.lexend
 
-//@Composable
-//fun DetailsBookScreen(){
-//}
 @Composable
 fun DetailsBookScreen(navController: NavController, books: String?) {
 
     val synopsis = "Synopsis"
 
     val book = books?.let { it1 -> BookRepository.findBook(it1) }
-
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
-        Card(
-            modifier = Modifier
-                .width(382.dp)
-                .height(522.dp)
-                .padding(vertical = 20.dp, horizontal = 15.dp),
-            shape = Shapes.large,
-            elevation = 15.dp,
-            border = BorderStroke(2.dp, Color(0xff757373)),
-            backgroundColor = MaterialTheme.colors.onSurface
-        ) {
-            if (book != null) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(book.imageUrl)
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    filterQuality = FilterQuality.High
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Transparent, elevation = 0.dp) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    tint = MaterialTheme.colors.primaryVariant,
+                    contentDescription = "Arrow Back",
+                    modifier = Modifier
+                        .clickable {
+                            navController.popBackStack()
+                        }
+                        .size(width = 25.dp, height =  25.dp)
                 )
             }
         }
+    }) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())) {
+            Card(
+                modifier = Modifier
+                    .width(382.dp)
+                    .height(522.dp)
+                    .padding(vertical = 20.dp, horizontal = 15.dp)
+                    .wrapContentWidth(),
+                shape = Shapes.large,
+                elevation = 15.dp,
+                border = BorderStroke(2.dp, Color(0xff757373)),
+                backgroundColor = MaterialTheme.colors.onSurface
+            ) {
+                if (book != null) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(book.imageUrl)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        filterQuality = FilterQuality.High
+                    )
+                }
+            }
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp, start = 15.dp, end = 15.dp)
-        ) {
-            Text(
-                text = synopsis,
-                style = MaterialTheme.typography.h4,
-                fontWeight = FontWeight.SemiBold,
-                color = MaterialTheme.colors.primaryVariant,
-            )
-
-            Spacer(modifier = Modifier.height(5.dp))
-
-            if (book != null) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, start = 15.dp, end = 15.dp)
+            ) {
                 Text(
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    text = book.synopsis,
-                    fontFamily = lexend,
-                    textAlign = TextAlign.Justify
+                    text = synopsis,
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colors.primaryVariant,
                 )
+
+                Spacer(modifier = Modifier.height(5.dp))
+
+                if (book != null) {
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = book.synopsis,
+                        fontFamily = lexend,
+                        textAlign = TextAlign.Justify
+                    )
+                }
             }
         }
     }
